eed788760c526d0ad9d764c208bd6345c57c76ef
Jimi Lindström <68616536+Koskajimi@users.noreply.github.com>
2021-02-16 18:38:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -84,11 +84,7 @@ function getNewToken(oAuth2Client, callback) {
   });
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
+
 
 function tulostaViimeinenRivi(auth) { //Gets the row we want the bot to post from the google sheet.
   const sheets = google.sheets({version: 'v4', auth});
